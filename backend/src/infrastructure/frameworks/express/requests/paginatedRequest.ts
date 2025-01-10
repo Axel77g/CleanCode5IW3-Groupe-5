46717646610fae23fa95ad1f8af096851f0c0fd0
Axel <axel@reality-academy.fr>
2025-01-11 00:14:38 +0100

    fix

--- backend/src/infrastructure/frameworks/express/requests/paginatedRequest.ts
+++ backend/src/infrastructure/frameworks/express/requests/paginatedRequest.ts
@@ -1,6 +1,9 @@
 import {z} from "zod";
 
-export const paginatedRequest = z.object({
+
+export const paginatedZodObject = {
     limit: z.string().default("10").transform((v) => Math.min(parseInt(v),20) || 10),
     page: z.string().default("1").transform((v) => parseInt(v) || 1),
-})
+}
+
+export const paginatedRequest = z.object(paginatedZodObject)
--- backend/src/infrastructure/frameworks/express/requests/paginatedWithDriverLicenseIdRequest.ts
+++ backend/src/infrastructure/frameworks/express/requests/paginatedWithDriverLicenseIdRequest.ts
@@ -1,7 +1,7 @@
 import {z} from "zod";
+import {paginatedZodObject} from "@expressApp/requests/paginatedRequest";
 
 export const paginatedWithDriverLicenseIdRequest = z.object({
     driverLicenseId : z.string(),
-    limit: z.string().default("10").transform((v) => Math.min(parseInt(v),20) || 10),
-    page: z.string().default("1").transform((v) => parseInt(v) || 1),
+    ...paginatedZodObject
 })
