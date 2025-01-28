00d202940cbf66158b31b0259ee7329993217c22
Ilyam Dupuis <ilyamdupuis0903@gmail.com>
2025-01-28 17:47:09 +0100

    feat(refacto): update Customer entity with new logic

--- backend/src/domain/maintenance/entities/Customer.ts
+++ backend/src/domain/maintenance/entities/Customer.ts
@@ -1,5 +1,8 @@
-import { AddressDTO, Address } from "@domain/shared/value-object/Address";
-import {ApplicationException} from "@shared/ApplicationException";
+import { Address, AddressDTO } from "@domain/shared/value-object/Address";
+import { ApplicationException } from "@shared/ApplicationException";
+import { RegisterCustomerEvent } from "../events/customer/RegisterCustomerEvent";
+import { UnregisterCustomerEvent } from "../events/customer/UnregisterCustomerEvent";
+import { UpdateCustomerEvent } from "../events/customer/UpdateCustomerEvent";
 
 export interface CustomerDTO {
     customerId: string;
@@ -18,9 +21,35 @@ export class Customer {
         public readonly address: Address,
     ) { }
 
+    registerEvent(): RegisterCustomerEvent {
+        return new RegisterCustomerEvent({
+            customerId: this.customerId,
+            name: this.name,
+            phoneNumber: this.phoneNumber,
+            email: this.email,
+            address: this.address,
+        });
+    }
+
+    updateEvent(): UpdateCustomerEvent {
+        return new UpdateCustomerEvent({
+            customerId: this.customerId,
+            name: this.name,
+            phoneNumber: this.phoneNumber,
+            email: this.email,
+            address: this.address,
+        })
+    }
+
+    unregisterEvent(): UnregisterCustomerEvent {
+        return new UnregisterCustomerEvent({
+            customerId: this.customerId,
+        })
+    }
+
     static fromObject(object: CustomerDTO): Customer | ApplicationException {
         const address = Address.create(object.address);
-        if (address instanceof Error) return address;
+        if (address instanceof ApplicationException) return address;
         return new Customer(
             object.customerId,
             object.name,
@@ -28,5 +57,15 @@ export class Customer {
             object.email,
             address,
         )
+    }
+
+    static create(customer: {
+        customerId: string,
+        name: string,
+        phoneNumber: string,
+        email: string,
+        address: Address
+    }) {
+        return new Customer(customer.customerId, customer.name, customer.phoneNumber, customer.email, customer.address);
     }
 }
